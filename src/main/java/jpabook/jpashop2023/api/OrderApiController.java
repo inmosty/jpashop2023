b6fd74ac8d713cf87ba1d5c7cd898172e982e623
jkpark <inmosty@gmail.com>
2023-03-20 22:01:48 +0900

    add api

--- src/main/java/jpabook/jpashop2023/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop2023/api/OrderApiController.java
@@ -6,23 +6,31 @@ import jpabook.jpashop2023.domain.OrderItem;
 import jpabook.jpashop2023.domain.OrderStatus;
 import jpabook.jpashop2023.repository.OrderRepository;
 import jpabook.jpashop2023.repository.OrderSearch;
+import jpabook.jpashop2023.repository.order.query.OrderFlatDto;
+import jpabook.jpashop2023.repository.order.query.OrderItemQueryDto;
+import jpabook.jpashop2023.repository.order.query.OrderQueryDto;
+import jpabook.jpashop2023.repository.order.query.OrderQueryRepository;
 import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.weaver.ast.Or;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.*;
+
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class OrderApiController {
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
 
     @GetMapping("/api/v1/orders")
     public List<Order> ordersV1(){
@@ -39,8 +47,43 @@ public class OrderApiController {
     @GetMapping("/api/v2/orders")
     public List<OrderDto> ordersV2(){
         List<Order> orders = orderRepository.findAllByString(new OrderSearch());
-        List<OrderDto> collect = orders.stream().map(o -> new OrderDto(o)).collect(Collectors.toList());
-        return collect;
+        List<OrderDto> result = orders.stream().map(o -> new OrderDto(o)).collect(toList());
+        return result;
+    }
+
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> orderV3(){
+        List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> result = orders.stream().map(o -> new OrderDto(o)).collect(toList());
+        return result;
+    }
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> orderV3_page(@RequestParam(value = "offset",defaultValue = "0") int offset
+                                        ,@RequestParam(value = "limit",defaultValue = "100") int limit
+                                       ){
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset,limit);
+        List<OrderDto> result = orders.stream().map(o -> new OrderDto(o)).collect(toList());
+        return result;
+    }
+
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> ordersV4(){
+        return orderQueryRepository.findOrderQueryDtos();
+    }
+
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5(){
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+    @GetMapping("/api/v6/orders")
+    public List<OrderQueryDto> ordersV6(){
+        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+        return flats.stream()
+                .collect(groupingBy(o -> new OrderQueryDto(o.getOrderId(), o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
+                        mapping(o -> new OrderItemQueryDto(o.getOrderId(), o.getItemName(), o.getOrderPrice(), o.getCount()), toList())
+                )).entrySet().stream()
+                .map(e -> new OrderQueryDto(e.getKey().getOrderId(), e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(), e.getKey().getAddress(), e.getValue()))
+                .collect(toList());
     }
 
     @Data
@@ -61,7 +104,7 @@ public class OrderApiController {
 /*            order.getOrderItems().stream().forEach(o->o.getItem().getName());
             this.orderItems = order.getOrderItems();*/
             this.orderItems = order.getOrderItems().stream()
-                    .map(orderItem -> new OrderItemDto(orderItem)).collect(Collectors.toList());
+                    .map(orderItem -> new OrderItemDto(orderItem)).collect(toList());
         }
     }
 
--- src/main/java/jpabook/jpashop2023/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop2023/repository/order/query/OrderQueryRepository.java
@@ -36,7 +36,7 @@ public class OrderQueryRepository {
      */
     private List<OrderQueryDto> findOrders() {
         return em.createQuery(
-                "select new jpabook.jpashop.repository.order.query.OrderQueryDto(o.id, m.name, o.orderDate, o.status, d.address)" +
+                "select new jpabook.jpashop2023.repository.order.query.OrderQueryDto(o.id, m.name, o.orderDate, o.status, d.address)" +
                         " from Order o" +
                         " join o.member m" +
                         " join o.delivery d", OrderQueryDto.class)
@@ -48,7 +48,7 @@ public class OrderQueryRepository {
      */
     private List<OrderItemQueryDto> findOrderItems(Long orderId) {
         return em.createQuery(
-                "select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
+                "select new jpabook.jpashop2023.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
                         " from OrderItem oi" +
                         " join oi.item i" +
                         " where oi.order.id = : orderId", OrderItemQueryDto.class)
@@ -84,7 +84,7 @@ public class OrderQueryRepository {
 
     private Map<Long, List<OrderItemQueryDto>> findOrderItemMap(List<Long> orderIds) {
         List<OrderItemQueryDto> orderItems = em.createQuery(
-                "select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
+                "select new jpabook.jpashop2023.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
                         " from OrderItem oi" +
                         " join oi.item i" +
                         " where oi.order.id in :orderIds", OrderItemQueryDto.class)
@@ -97,7 +97,7 @@ public class OrderQueryRepository {
 
     public List<OrderFlatDto> findAllByDto_flat() {
         return em.createQuery(
-                "select new jpabook.jpashop.repository.order.query.OrderFlatDto(o.id, m.name, o.orderDate, o.status, d.address, i.name, oi.orderPrice, oi.count)" +
+                "select new jpabook.jpashop2023.repository.order.query.OrderFlatDto(o.id, m.name, o.orderDate, o.status, d.address, i.name, oi.orderPrice, oi.count)" +
                         " from Order o" +
                         " join o.member m" +
                         " join o.delivery d" +
